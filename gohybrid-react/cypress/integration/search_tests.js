90c5c72d1f08d1c004d2c7b6ce28920a8d176692
RJ Salamanca <salamancaronito@gmail.com>
2020-01-29 15:47:18 -0500

    added semicolons

--- gohybrid-react/cypress/integration/search_tests.js
+++ gohybrid-react/cypress/integration/search_tests.js
@@ -2,7 +2,7 @@ const currentYear = new Date().getFullYear()
 
 context('Main Search Tests', () => {
    beforeEach(() => {
-      cy.visit('http://localhost:3001')
+      cy.visit('http://localhost:3001');
    })
 
    it('Selecting Year Alone', () => {
@@ -18,7 +18,7 @@ context('Main Search Tests', () => {
       // By default has a length of 2.
       cy.get('#selectMake')
          .children()
-         .should('have.length', 2)
+         .should('have.length', 2);
    })
 
    describe('Selecing Different Year and Make', () => {
@@ -34,7 +34,7 @@ context('Main Search Tests', () => {
             .should('be.visible')
             .children((child) => {
                //should have children!
-               expect(child).to.be.greaterThan(0)
+               expect(child).to.be.greaterThan(0);
             });
       });
 
@@ -43,12 +43,12 @@ context('Main Search Tests', () => {
             .select(`${currentYear}`).invoke('val');
 
          cy.get('#selectMake')
-            .select('Alfa Romeo').invoke('val')
+            .select('Alfa Romeo').invoke('val');
 
          cy.get('.carModels')
             .should('not.be.visible');
 
-         cy.contains('No Hybrids Found')
+         cy.contains('No Hybrids Found');
       })
    })
 });
